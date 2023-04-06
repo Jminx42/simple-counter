909b01cb7b2c92dfdc6b523928be9f90ff5c1d1f
Jminx42 <visagiejessica@gmail.com>
2023-04-06 18:23:19 +0000

    finished the 3 buttons and logic behind them, show/hide, play/pause, reset

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,58 @@ import SecondsBlock from "./secondsblock.jsx";
 //create your first component
 const Home = () => {
 const [timer, setTimer] = useState(0);
+const [isPlay, setIsPlay] = useState(true);
+const [showButtons, setShowButtons] = useState(true);
 
 useEffect (() => {
+	
 	const interval = setInterval(() => {
+		if (isPlay) {
 		setTimer(timer => timer + 1);
+		}
 	}, 1000);
-}, []);
+	
+	return () => clearInterval(interval);	
+}, [isPlay]);
 
 	return (
 		<div className="container">
-			<SecondsBlock time={<i class="fa-regular fa-clock"></i>}/>
-			<SecondsBlock time={0}/>
-			<SecondsBlock time={0}/>
-			<SecondsBlock time={0}/>
-			<SecondsBlock time={Math.floor(timer * 10) % 10}/>
-			<SecondsBlock time={Math.floor(timer) % 10}/>
+			<div className="row m-3 justify-content-center text-center">
+				<div className="col-4">
+				<button className="btn btn-warning" onClick={() => {
+					setShowButtons(!showButtons);
+				}}>{showButtons ? "HIDE" : "SHOW"}</button>
+				</div>
+			</div>
+			<div className="row bg-dark justify-content-center align-items-center">
+				<SecondsBlock time={<i class="fa-regular fa-clock"></i>}/>
+				<SecondsBlock time={Math.floor(timer / 10000) % 10}/>
+				<SecondsBlock time={Math.floor(timer / 1000) % 10}/>
+				<SecondsBlock time={Math.floor(timer / 100) % 10}/>
+				<SecondsBlock time={Math.floor(timer / 10) % 10}/>
+				<SecondsBlock time={Math.floor(timer) % 10}/>
+			</div>
+			{
+				showButtons ?
+				<>
+				<div className="row m-3 justify-content-center text-center">
+				<div className="col-4 ">
+					<button className={"btn btn-" + (isPlay ? "danger" : "success")} onClick={() => {
+						setIsPlay(!isPlay);
+						}}>{isPlay ? "PAUSE" : "PLAY"}</button>
+				</div>
+				<div className="col-4 ">
+					<button className="btn btn-secondary" onClick={() => {
+						setTimer(0);
+						setIsPlay(false);
+					}}>RESET</button>
+				</div>
+				</div>
+				</>
+				: null
+			}
+			
+			
 		</div>
 	);
 };
